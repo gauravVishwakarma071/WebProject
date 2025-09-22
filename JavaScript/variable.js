add8b2edb40ce3fc2fe8ea285b99aa29bd24b4f0
Gaurav <gauravvishwakarma071@gmail.com>
2025-09-22 23:26:13 +0530

    variables in JS

---
+++ JavaScript/variable.js
@@ -0,0 +1,13 @@
+// var (old way)
+var x = 10;
+console.log(x); // 10
+
+// let (modern way)
+let y = 20;
+y = 25;  // allowed
+console.log(y); // 25
+
+// const (best for fixed values)
+const z = 30;
+// z = 40; // ❌ Error: Assignment to constant variable
+console.log(z);
