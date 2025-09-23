676c4ccafb4a36322006add7e3866265f2a37c78
Gaurav <gauravvishwakarma071@gmail.com>
2025-09-23 23:53:43 +0530

    array in js.

---
+++ JavaScript/part-1/array.js
@@ -0,0 +1,10 @@
+//Array Declaration
+// Using square brackets
+let fruits = ["Apple", "Banana", "Mango"];
+
+// Using Array constructor
+let numbers = new Array(1, 2, 3, 4);
+
+
+console.log(fruits)
+console.log(numbers)
