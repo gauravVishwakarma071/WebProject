2dd3c29bcb97404cc63e2c84fa5cfbfabad2cff5
Gaurav <gauravvishwakarma071@gmail.com>
2025-09-24 12:04:22 +0530

    Learing Dom manipulation.

--- JavaScript/part-2/TheDom/the_dom.js
+++ JavaScript/part-2/TheDom/the_dom.js
@@ -4,3 +4,17 @@ h1.innerText = "Okk I know!";
 h1.innerHTML = "<i>Hy I am gaurav</i>"
 
 // h1.hidden = true;
+
+
+let a = document.querySelector("a");
+a.setAttribute("href", "https://www.google.com");
+
+//Image-tag
+let img = document.querySelector("img");
+img.setAttribute("src", "photo.jpg");
+img.height = "100";
+img.width = "200";
+
+//p-tag
+let p = document.querySelector("p");
+p.removeAttribute("");
