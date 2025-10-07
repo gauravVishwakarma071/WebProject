60eff8004339e7521859c6160fb0b56f1024a9c8
Gaurav <gauravvishwakarma071@gmail.com>
2025-10-07 21:56:09 +0530

    Adding and removing event Listeners.

--- JavaScript/part-2/Event/event.js
+++ JavaScript/part-2/Event/event.js
@@ -1,4 +1,17 @@
 let apple = document.querySelector("h2");
-apple.addEventListener("click", function(){
+apple.addEventListener("click", function () {
     apple.style.color = "red";
-}); 
+});
+
+let p = document.querySelector("p");
+
+function yellow(){
+    p.style.color = "yellow";
+};
+function red(){
+    apple.style.color = "red";
+};
+
+// p.addEventListener("click",yellow);
+p.addEventListener("dblclick",red);
+// p.removeEventListener("dblclick", yellow);
