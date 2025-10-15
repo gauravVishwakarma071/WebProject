7002eb1390a9638f8731fafa308a11ac2f2d5e7f
Gaurav <gauravvishwakarma071@gmail.com>
2025-10-15 08:34:03 +0530

    Generating card with "Form" info is done.

--- JavaScript/part-2/Event/Form/form.js
+++ JavaScript/part-2/Event/Form/form.js
@@ -1,6 +1,7 @@
 // form.js
+let main = document.querySelector("#main");
 let form = document.querySelector("#myForm");
-let inputs = document.querySelectorAll("input")
+let inputs = document.querySelectorAll("input");
 
 form.addEventListener("submit", function (dets) {
     dets.preventDefault();
@@ -12,8 +13,33 @@ form.addEventListener("submit", function (dets) {
     profile.classList.add("profile");
 
     let img = document.createElement("img");
-    img.setAttribute(
-        "src",
-        "my_pic.jpeg"
-    );
+    img.setAttribute("src",inputs[0].value);
+
+    let h3 = document.createElement("h3");
+    h3.textContent = inputs[1].value; 
+    let h5 = document.createElement("h5");
+    h5.textContent = inputs[2].value;
+    let hr = document.createElement("hr"); 
+    let p = document.createElement("p"); 
+    p.textContent = inputs[3].value;
+
+
+    inputs.forEach(function(inp){
+        if(inp.type !== "submit") inp.value = "";
+    });
+
+    // "Hy,, everyone i am gaurav a MCA final year student and a aspring full stack java developer, I am proficient in java, kotin, javaScript, SQL. I am also aware of android app and have build some projects like chat application and live streaming platfrom. I have also good understanding of WEB tech including HTML5, CSS, javaScript. currenty i am learning Spring boot and React to level up my skills.
+
+
+
+
+    profile.appendChild(img);
+    card.appendChild(profile);
+    card.appendChild(h3);
+    card.appendChild(h5);
+    card.appendChild(hr);
+    card.appendChild(p);
+
+    main.appendChild(card);
+
 });
