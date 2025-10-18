6afe818afe29a1bb4897fccaad86ae76ffb5a2b2
Gaurav <gauravvishwakarma071@gmail.com>
2025-10-18 07:56:44 +0530

    event bubbling + toggle to add and remove line-thruogh on a <li>

--- JavaScript/part-2/Event/bubbling/bubble.js
+++ JavaScript/part-2/Event/bubbling/bubble.js
@@ -1,3 +1,8 @@
 document.querySelector("#nav").addEventListener("click", function(){
     alert("NAV clicked");
 });
+
+let ul = document.querySelector("ul");
+ul.addEventListener("click", function(dets){
+    dets.target.classList.toggle("lt");
+});
