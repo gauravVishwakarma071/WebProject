dd1e3e7ac7619437467da7687be59f03f55120db
Gaurav <gauravvishwakarma071@gmail.com>
2025-10-25 09:49:45 +0530

    reading inputs from form - JS

---
+++ JavaScript/part-2/Form&Validation/ReadVaule/read_value.js
@@ -0,0 +1,13 @@
+let naam = document.querySelector("#name");
+let form = document.querySelector("#userForm");
+
+form.addEventListener("submit", function (dets) {
+  dets.preventDefault();
+
+  if (naam.value.length <= 2) {
+document.querySelector("#hide").style.display = "initial";
+} else {
+    document.querySelector("#hide").style.display = "none";
+   
+  }
+});
