a7e7ca3b638b036c175823e325665f13031e6c06
Gaurav <gauravvishwakarma071@gmail.com>
2025-10-27 12:24:13 +0530

    setting timer and interval in js

--- JavaScript/part-2/Timer&Intervals/set_clear_Timer.html/script.js
+++ JavaScript/part-2/Timer&Intervals/set_clear_Timer.html/script.js
@@ -1,7 +1,13 @@
-setTimeout(function(){
+let tm = setTimeout(function(){
     console.log("hello")
 }, 5000);
 
-setInterval(function(){
+
+let Im = setInterval(function(){
     console.log("JS timer");
 },3000);
+
+clearTimeout(tm);
+clearInterval(Im);
+
+
