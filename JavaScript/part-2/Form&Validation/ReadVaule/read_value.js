0662e105e105dccba90b9c7ff0982307a3852f94
Gaurav <gauravvishwakarma071@gmail.com>
2025-10-27 07:27:34 +0530

    used regex code to validate email and username.

--- JavaScript/part-2/Form&Validation/ReadVaule/read_value.js
+++ JavaScript/part-2/Form&Validation/ReadVaule/read_value.js
@@ -2,7 +2,19 @@ let naam = document.querySelector("#name");
 let form = document.querySelector("#userForm");
 
 form.addEventListener("submit", function (dets) {
-    dets.preventDefault();
+    dets.preventDefault(); 
+
+
+    //checks valid username
+    const v_username = /^[a-zA-Z0-9._-]{3,20}$/;
+    let vname = v_username.test("gaurav@xd_");
+    console.log(vname);
+
+    //checks valid email
+    const v_email = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    let vemail = v_email.test("gm@a.m")
+    console.log(vemail);    
+
 
     if (naam.value.length <= 2) {
         document.querySelector("#hide").style.display = "initial";
