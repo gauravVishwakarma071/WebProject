5ced2d9398e054351b10a2fca83700b4484d5f9a
Gaurav <gauravvishwakarma071@gmail.com>
2025-11-08 23:57:07 +0530

    connected to MySql sever and created a database

---
+++ Assignment-4/std_form_2.0.php
@@ -0,0 +1,22 @@
+<?php
+
+//connect to MySQL server
+$servername = "localhost";
+$usernsme = "root";
+$password = "";
+
+$conn = mysqli_connect($servername,$usernsme,$password);
+if(!conn){
+    die("Connection failed: " .mysqli_connect_error());
+}
+echo "Connect to MySQL server successfully<br>";
+
+//Create a database
+$dbname = "user_db";
+$sql = "CREATE DATABASE IF NOT EXISTS $dbname";
+if(mysqli_query($conn,$sql)){
+    echo "Database '$dbname' created successfully<br>";
+}else{
+    die("Error creating database:" .mysqli_error($conn));
+}
+?>
