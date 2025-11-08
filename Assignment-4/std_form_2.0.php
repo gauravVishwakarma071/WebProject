dcf5ce0b6f99023a435cf0a833f94306acf607b2
Gaurav <gauravvishwakarma071@gmail.com>
2025-11-09 00:14:30 +0530

    created a table and inserted some example data in it for testing.

--- Assignment-4/std_form_2.0.php
+++ Assignment-4/std_form_2.0.php
@@ -23,4 +23,30 @@ if(mysqli_query($conn,$sql)){
 //Select the database
 mysqli_select_db($conn,$dbname);
 echo "Using database '$dbname'<br>";
+
+//Create table
+$sql = "CREATE TABLE IF NOT EXISTS user(
+id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+name VARCHAR(50) NOT NULL,
+email VARCHAR(500) NOT NULL
+)";if(mysqli_query($conn,$sql)){
+    echo "Table 'users' created successfully<br>";
+}else{
+    die("Error creating table: " .mysqli_error($conn));
+}
+
+//Insert some data
+$sql = "INSERT INTO users (name, email) 
+VALUES
+('ACB', 'abc@example.com'),
+('XYZ', 'xyz@example.com'),
+";
+
+if(mysqli_query($conn,$sql)){
+    echo "Data inserted sucessfully<br>";
+}else{
+    die("Error inserting data" .mysqli_error($conn));
+}
+
+
 ?>
