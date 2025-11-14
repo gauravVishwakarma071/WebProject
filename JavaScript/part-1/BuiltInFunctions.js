36682cb1d7118acb5f16740ced2ddc5a2bdc596b
Gaurav <gauravvishwakarma071@gmail.com>
2025-11-15 00:15:38 +0530

    Some built-in Fuctions of JS

---
+++ JavaScript/part-1/BuiltInFunctions.js
@@ -0,0 +1,17 @@
+// Math function 
+var result = Math.sqrt(25); // returns 5 
+
+// String function 
+var text = "JavaScript";
+console.log(text.toUpperCase()); // JAVASCRIPT 
+
+// Number function 
+var num = parseInt("123"); // converts string to number
+
+
+alert() //→ Displays a message box to the user. 
+parseInt() //→ Converts a string into an integer. 
+parseFloat() //→ Converts a string into a floating - point number. 
+isNaN() // Checks whether a value is “Not a Number”. 
+Math.round() //→ Rounds a number to the nearest integer. 
+Date() //→ Returns the current date and time. 
