61889296d57c35baced6b9b1e360cacc343c351a
Gaurav <gauravvishwakarma071@gmail.com>
2025-11-14 23:39:58 +0530

    Nested if_else

---
+++ JavaScript/part-1/nested_if.js
@@ -0,0 +1,13 @@
+var age = 25; var hasLicense = true;
+
+if (age >= 18) {
+    if (hasLicense) {
+        console.log("You can drive a car.");
+    }
+    else {
+        console.log("You need a driving license.");
+    }
+}
+else {
+    console.log("You are too young to drive.");
+} 
