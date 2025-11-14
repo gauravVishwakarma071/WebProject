c94076a23f753df004ba51ee3879fa001ec3328b
Gaurav <gauravvishwakarma071@gmail.com>
2025-11-14 23:46:01 +0530

    Loops in JS

---
+++ JavaScript/part-1/Loops.js
@@ -0,0 +1,19 @@
+//For Loop
+for (var i = 1; i <= 5; i++) {
+    console.log("Number: " + i);
+}
+
+
+//While Loop
+var count = 1;
+while (count <= 5) {
+    console.log("Count is: " + count);
+    count++;
+}
+
+//DO-While Loop
+var num = 6; do {
+    console.log("Number is: " + num);
+    num++;
+}
+while (num <= 5); 
