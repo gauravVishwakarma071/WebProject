ebf52d079009adbb5740ee4033b99d533dea4576
Gaurav <gauravvishwakarma071@gmail.com>
2025-11-18 23:52:40 +0530

    An object is a collection of key–value pairs.

---
+++ JavaScript/part-1/object.js
@@ -0,0 +1,13 @@
+//An object is a collection of key–value pairs
+
+var car = {
+    company : "BMW",
+    model : "1945",
+    color : "Black"
+}
+
+console.log(car.company);
+
+car.owner = "gaurav";
+
+console.log(car.owner);
