86a7c2361fe83454f55908f04da0cef4ac498604
Gaurav <gauravvishwakarma071@gmail.com>
2025-11-19 22:19:30 +0530

    JSON is a lightweight, text-based format for storing and exchanging data.

---
+++ JavaScript/part-1/JSON.js
@@ -0,0 +1,10 @@
+//JSON (JavaScript Object Notation) is a lightweight, text-based format for storing and exchanging data.
+
+{
+    "name": "Gaurav",
+    "age": 22,
+    "course": "MCA"
+}
+
+var jsObj = { name: "Amit", age: 25 }; // JavaScript Object 
+var jsonStr = '{"name":"Amit","age":25}'; // JSON String 
